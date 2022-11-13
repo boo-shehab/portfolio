dd74956b4a9612fe3f33aef6b5003ddea1433e31
ahmed-bushhab <bfhetyrhyr12@gmail.com>
2022-11-13 10:21:21 +0300

    build demo

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,10 +9,16 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/image', to: 'image' },
+      ],
+    }),
   ],
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'src/assets/images/[name].[ext]',
     clean: true,
   },
   devServer: {
@@ -25,9 +32,10 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+        type: 'javascript/auto',
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
         type: 'asset/resource',
       },
     ],
